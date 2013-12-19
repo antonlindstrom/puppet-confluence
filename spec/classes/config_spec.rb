669874a6328e66583d4e545a885f93f90d93163b
Anton Lindström <lindztr@gmail.com>
2013-12-19 15:08:57 +0000

    add test for confluence-init.properties

--- spec/classes/config_spec.rb
+++ spec/classes/config_spec.rb
@@ -7,4 +7,5 @@ describe 'confluence::config' do
   it { should contain_user('confluence') }
   it { should contain_file('/home/confluence') }
   it { should contain_file('setenv.sh') }
+  it { should contain_file('confluence-init.properties') }
 end
